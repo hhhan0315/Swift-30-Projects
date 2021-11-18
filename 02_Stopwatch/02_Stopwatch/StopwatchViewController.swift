6464ab949ce8c4b4a0ca48e0065cd2914e36fda0
hhhan0315 <hhan0315@naver.com>
2021-11-18 23:28:14 +0900

    프로젝트2 Stopwatch 생성 및 SceneDelegate 설정

---
+++ 02_Stopwatch/02_Stopwatch/StopwatchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StopwatchViewController.swift
+//  02_Stopwatch
+//
+//  Created by rae on 2021/11/18.
+//
+
+import UIKit
+
+class StopwatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
+
