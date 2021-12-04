52f88ed6724eb81b2ef64cdcedb10da40c27e566
hhhan0315 <hhan0315@naver.com>
2021-12-04 17:54:38 +0900

    프로젝트7 생성

---
+++ 07_PokedexGo/07_PokedexGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  07_PokedexGo
+//
+//  Created by rae on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
