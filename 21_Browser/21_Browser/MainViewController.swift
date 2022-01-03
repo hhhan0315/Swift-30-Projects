4a7aaa61f78a24b7a9dc51fed6766d921e00ef9f
hhhan0315 <hhan0315@naver.com>
2022-01-03 15:50:17 +0900

    WKWebView 구현

--- 21_Browser/21_Browser/MainViewController.swift
+++ 21_Browser/21_Browser/MainViewController.swift
@@ -14,29 +14,64 @@ class MainViewController: UIViewController {
         let webView = WKWebView()
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.navigationDelegate = self
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         return webView
     }()
     
-    //    private let progressView: UIProgressView = {
-    //        let progressView = UIProgressView()
-    //        progressView.translatesAutoresizingMaskIntoConstraints = false
-    //        progressView.backgroundColor = .red
-    //        return progressView
-    //    }()
+    private lazy var urlField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: view.frame.width * 0.7, height: 0))
+        textField.placeholder = "https://"
+        textField.borderStyle = .roundedRect
+        textField.delegate = self
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
     
-    private var backButton = UIBarButtonItem()
-    private var forwardButton = UIBarButtonItem()
-    private var reloadButton = UIBarButtonItem()
+    private lazy var progressView: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .default)
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.progress = 1
+        return progressView
+    }()
+    
+    private var backButton: UIBarButtonItem = {
+        let barButton = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .plain, target: self, action: #selector(touchBackButton(_:)))
+        barButton.isEnabled = false
+        return barButton
+    }()
+    
+    private var forwardButton: UIBarButtonItem = {
+        let barButton = UIBarButtonItem(image: UIImage(systemName: "arrow.right"), style: .plain, target: self, action: #selector(touchForwardButton(_:)))
+        barButton.isEnabled = false
+        return barButton
+    }()
+    
+    private var reloadButton: UIBarButtonItem = {
+        let barButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(touchReloadButton(_:)))
+        return barButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
+        setNavigationBar()
         setToolBar()
         addViews()
         autoLayout()
         loadWebView(urlString: "https://www.apple.com")
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+            progressView.isHidden = (webView.estimatedProgress == 1)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        urlField.endEditing(true)
     }
     
     private func addViews() {
@@ -59,10 +94,21 @@ class MainViewController: UIViewController {
         webView.load(urlRequest)
     }
     
+    private func setNavigationBar() {
+        navigationItem.titleView = urlField
+        
+        guard let titleView = navigationItem.titleView else { return }
+        
+        titleView.addSubview(progressView)
+        
+        NSLayoutConstraint.activate([
+            progressView.bottomAnchor.constraint(equalTo: titleView.bottomAnchor),
+            progressView.leadingAnchor.constraint(equalTo: titleView.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: titleView.trailingAnchor),
+        ])
+    }
+    
     private func setToolBar() {
-        backButton = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .plain, target: self, action: #selector(touchBackButton(_:)))
-        forwardButton = UIBarButtonItem(image: UIImage(systemName: "arrow.right"), style: .plain, target: self, action: #selector(touchForwardButton(_:)))
-        reloadButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(touchReloadButton(_:)))
         let flexibleSpace = UIBarButtonItem(systemItem: .flexibleSpace)
         let toolBarItems = [backButton, forwardButton, flexibleSpace, reloadButton]
         
@@ -87,5 +133,29 @@ extension MainViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         backButton.isEnabled = webView.canGoBack
         forwardButton.isEnabled = webView.canGoForward
+        progressView.setProgress(0.0, animated: false)
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        let alert = UIAlertController(title: "오류", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension MainViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard var text = textField.text else {
+            return false
+        }
+        
+        if !text.hasPrefix("https://") {
+            text.insert(contentsOf: "https://", at: text.startIndex)
+        }
+        
+        loadWebView(urlString: text)
+        
+        textField.resignFirstResponder()
+        return true
     }
 }
